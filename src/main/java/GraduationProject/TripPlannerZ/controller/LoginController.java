ec27e65a9466f9b2bc81e4b7af6933f01cbcee21
Lee-Dongwook <dlehddnr0713@gmail.com>
2023-05-03 17:08:03 +0900

    [Feat]Logout couple

--- src/main/java/GraduationProject/TripPlannerZ/controller/LoginController.java
+++ src/main/java/GraduationProject/TripPlannerZ/controller/LoginController.java
@@ -53,9 +53,6 @@ public class LoginController {
     public void logout(HttpServletRequest request) {
         HttpSession session = request.getSession(false);
 
-        Member member = (Member) session.getAttribute("loginMember");
-        System.out.println("member.getEmail() = " + member.getEmail());
-
         if (session != null)
             session.invalidate();
     }
