cba239000fc8db698ee5c51d79cc0afaba0fe109
cseongbo <cseongbo17@gmail.com>
2023-05-10 11:37:40 +0900

    TripController

---
+++ src/main/java/GraduationProject/TripPlannerZ/controller/TripController.java
@@ -0,0 +1,18 @@
+package GraduationProject.TripPlannerZ.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = {"http://localhost:3000", "http://localhost:8080"}, allowCredentials = "true")
+public class TripController {
+
+    @PostMapping()
+
+
+
+}
