d0117af19220948f6c2b900cf627f54484e0c749
Lee-Dongwook <dlehddnr0713@gmail.com>
2023-05-11 19:56:40 +0900

    [Feat] KakaoMap init

--- client/src/components/InitPage/InitPage.js
+++ client/src/components/InitPage/InitPage.js
@@ -144,136 +144,136 @@ function InitPage(){
     var map = new kakao.maps.Map(mapContainer, mapOption);
     var ps = new kakao.maps.services.Places();
     var infowindow = new kakao.maps.InfoWindow({zIndex:1});
-
-    function displayPlaces(places){
-       var listEl = document.getElementById('placesList');
-       var menuEl = document.getElementById('menu_wrap');
-       var fragment = document.createDocumentFragment();
-       var bounds = new kakao.maps.LatLngBounds();
-       var listStr = '';
-
-       removeAllChildNods(listEl);
-       removeMarker();
-
-       for(var i = 0; i<places.length; i++)
-       {
-          var placePosition = new kakao.maps.LatLng(places[i].y, places[i].x);
-          var marker = addMarker(placePosition, i);
-          var itemEl = getListItem(i,places[i]);
-          bounds.extend(placePosition);
-
-          (function(marker,title){
-             kakao.maps.event.addListener(marker, 'mouseover', function(){
-                displayInfowindow(marker,title);
-             });
-
-             itemEl.onmouseover = function() {
-                displayInfowindow(marker, title);
-             };
-
-             itemEl.onmouseout = function(){
-                infowindow.close();
-             };
-          })(marker,places[i].place_name);
-
-          fragment.appendChild(itemEl);
-       }
-       listEl.appendChild(fragment);
-       menuEl.scrollTop = 0;
-
-       map.setBounds(bounds);
-    }
-    function getListItem(index,places){
-         var el = document.createElement('li'),
-            itemStr = '<span class="markerbg marker_' + (index+1) + '"></span>' +
-                        '<div class="info">' +
-                        '   <h5>' + places.place_name + '</h5>';
-
-            if (places.road_address_name) {
-                itemStr += '    <span>' + places.road_address_name + '</span>' +
-                            '   <span class="jibun gray">' +  places.address_name  + '</span>';
-            } else {
-                itemStr += '    <span>' +  places.address_name  + '</span>';
-            }
-
-              itemStr += '  <span class="tel">' + places.phone  + '</span>' +
-                        '</div>';
-
-            el.innerHTML = itemStr;
-            el.className = 'item';
-
-            return el;
-    }
-
-    function placesSearchCB(data, status, pagination){
-        if(status === kakao.maps.services.Status.OK){
-            displayPlaces(data);
-            displayPagination(pagination);
-        }
-        else if(status === kakao.maps.services.Status.ZERO_RESULT){
-            alert('검색 결과가 존재하지 않습니다.');
-            return;
-        }
-        else if(status === kakao.maps.services.Status.ERROR){
-            alert('검색 결과 중 오류가 발생했습니다.');
-            return;
-        }
-    }
-
-    function searchPlaces(){
-        var keyword = document.getElementById('keyword').value;
-        if(!keyword.replace(/^\s+|\s+$/g, '')){
-          alert('키워드를 입력해주세요!');
-          return false;
-        }
-        ps.keywordSearch(keyword, placesSearchCB);
-    }
-
-    searchPlaces();
-
-    function displayInfowindow(marker, title) {
-        var content = '<div style="padding:5px;z-index:1;">' + title + '</div>';
-
-        infowindow.setContent(content);
-        infowindow.open(map, marker);
-    }
-
-
-
-    function displayPagination(pagination) {
-        var paginationEl = document.getElementById('pagination'),
-            fragment = document.createDocumentFragment(),
-            i;
-
-        while (paginationEl.hasChildNodes()) {
-            paginationEl.removeChild (paginationEl.lastChild);
-        }
-
-        for (i=1; i<=pagination.last; i++) {
-            var el = document.createElement('a');
-            el.href = "#";
-            el.innerHTML = i;
-
-            if (i===pagination.current) {
-                el.className = 'on';
-            } else {
-                el.onclick = (function(i) {
-                    return function() {
-                        pagination.gotoPage(i);
-                    }
-                })(i);
-            }
-
-            fragment.appendChild(el);
-        }
-        paginationEl.appendChild(fragment);
-    }
-
-    function removeAllChildNods(el) {
-        while (el.hasChildNodes()) {
-            el.removeChild (el.lastChild);
-        }
-    }
+//
+//    function displayPlaces(places){
+//       var listEl = document.getElementById('placesList');
+//       var menuEl = document.getElementById('menu_wrap');
+//       var fragment = document.createDocumentFragment();
+//       var bounds = new kakao.maps.LatLngBounds();
+//       var listStr = '';
+//
+//       removeAllChildNods(listEl);
+//       removeMarker();
+//
+//       for(var i = 0; i<places.length; i++)
+//       {
+//          var placePosition = new kakao.maps.LatLng(places[i].y, places[i].x);
+//          var marker = addMarker(placePosition, i);
+//          var itemEl = getListItem(i,places[i]);
+//          bounds.extend(placePosition);
+//
+//          (function(marker,title){
+//             kakao.maps.event.addListener(marker, 'mouseover', function(){
+//                displayInfowindow(marker,title);
+//             });
+//
+//             itemEl.onmouseover = function() {
+//                displayInfowindow(marker, title);
+//             };
+//
+//             itemEl.onmouseout = function(){
+//                infowindow.close();
+//             };
+//          })(marker,places[i].place_name);
+//
+//          fragment.appendChild(itemEl);
+//       }
+//       listEl.appendChild(fragment);
+//       menuEl.scrollTop = 0;
+//
+//       map.setBounds(bounds);
+//    }
+//    function getListItem(index,places){
+//         var el = document.createElement('li'),
+//            itemStr = '<span class="markerbg marker_' + (index+1) + '"></span>' +
+//                        '<div class="info">' +
+//                        '   <h5>' + places.place_name + '</h5>';
+//
+//            if (places.road_address_name) {
+//                itemStr += '    <span>' + places.road_address_name + '</span>' +
+//                            '   <span class="jibun gray">' +  places.address_name  + '</span>';
+//            } else {
+//                itemStr += '    <span>' +  places.address_name  + '</span>';
+//            }
+//
+//              itemStr += '  <span class="tel">' + places.phone  + '</span>' +
+//                        '</div>';
+//
+//            el.innerHTML = itemStr;
+//            el.className = 'item';
+//
+//            return el;
+//    }
+//
+//    function placesSearchCB(data, status, pagination){
+//        if(status === kakao.maps.services.Status.OK){
+//            displayPlaces(data);
+//            displayPagination(pagination);
+//        }
+//        else if(status === kakao.maps.services.Status.ZERO_RESULT){
+//            alert('검색 결과가 존재하지 않습니다.');
+//            return;
+//        }
+//        else if(status === kakao.maps.services.Status.ERROR){
+//            alert('검색 결과 중 오류가 발생했습니다.');
+//            return;
+//        }
+//    }
+//
+//    function searchPlaces(){
+//        var keyword = document.getElementById('keyword').value;
+//        if(!keyword.replace(/^\s+|\s+$/g, '')){
+//          alert('키워드를 입력해주세요!');
+//          return false;
+//        }
+//        ps.keywordSearch(keyword, placesSearchCB);
+//    }
+//
+//    searchPlaces();
+//
+//    function displayInfowindow(marker, title) {
+//        var content = '<div style="padding:5px;z-index:1;">' + title + '</div>';
+//
+//        infowindow.setContent(content);
+//        infowindow.open(map, marker);
+//    }
+//
+//
+//
+//    function displayPagination(pagination) {
+//        var paginationEl = document.getElementById('pagination'),
+//            fragment = document.createDocumentFragment(),
+//            i;
+//
+//        while (paginationEl.hasChildNodes()) {
+//            paginationEl.removeChild (paginationEl.lastChild);
+//        }
+//
+//        for (i=1; i<=pagination.last; i++) {
+//            var el = document.createElement('a');
+//            el.href = "#";
+//            el.innerHTML = i;
+//
+//            if (i===pagination.current) {
+//                el.className = 'on';
+//            } else {
+//                el.onclick = (function(i) {
+//                    return function() {
+//                        pagination.gotoPage(i);
+//                    }
+//                })(i);
+//            }
+//
+//            fragment.appendChild(el);
+//        }
+//        paginationEl.appendChild(fragment);
+//    }
+//
+//    function removeAllChildNods(el) {
+//        while (el.hasChildNodes()) {
+//            el.removeChild (el.lastChild);
+//        }
+//    }
 
     function addMarker(position)
     {
