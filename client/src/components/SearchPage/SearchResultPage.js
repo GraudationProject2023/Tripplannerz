cfc127aef2191b413c0a597f6f70083b980c95af
Lee-Dongwook <dlehddnr0713@gmail.com>
2023-06-22 19:43:36 +0900

    [Fix] localStorage temporary State Control

--- client/src/components/SearchPage/SearchResultPage.js
+++ client/src/components/SearchPage/SearchResultPage.js
@@ -9,6 +9,13 @@ function SearchResultPage(props) {
   const [post, setPost] = useState([]);
   const [loading, setLoading] = useState(true);
 
+   useEffect(() => {
+                   localStorage.setItem("cast",1);
+                   localStorage.setItem("rank",-1);
+                   localStorage.setItem("vest",1);
+                   document.cookie = 'cookieName=JSESSIONID; expires=THU, 01 Jan 1970 00:00:00 UTC; path=/;'
+              },[]);
+
   useEffect(() => {
     const fetchPost = async () => {
       try {
