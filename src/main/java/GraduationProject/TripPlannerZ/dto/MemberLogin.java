81fd0dcef0a74f1c3c55d30ba1656b5776ee76dd
cseongbo <cseongbo17@gmail.com>
2023-06-28 18:42:59 +0900

    MyPage add

--- src/main/java/GraduationProject/TripPlannerZ/dto/MemberLogin.java
+++ src/main/java/GraduationProject/TripPlannerZ/dto/MemberLogin.java
@@ -1,4 +1,4 @@
-package GraduationProject.TripPlannerZ.controller;
+package GraduationProject.TripPlannerZ.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class MemberLoginDTO {
+public class MemberLogin {
 
     private String pw;
     private String email;
