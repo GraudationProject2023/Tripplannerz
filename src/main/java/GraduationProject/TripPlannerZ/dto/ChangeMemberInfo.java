172b8efaef38d52d53664158535df4c219c7b8da
YongHyunHong <108213846+YongHyunHong@users.noreply.github.com>
2023-07-11 22:41:43 +0900

    [feat] ChangeMemberPreferences Service

---
+++ src/main/java/GraduationProject/TripPlannerZ/dto/ChangeMemberInfo.java
@@ -0,0 +1,12 @@
+package GraduationProject.TripPlannerZ.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class ChangeMemberInfo {
+
+    private String pw;
+    private String types;
+}
