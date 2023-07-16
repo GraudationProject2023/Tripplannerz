9eccdb1aa0f0a612fd5a20f063eaa04e1501cc27
Lee-Dongwook <dlehddnr0713@gmail.com>
2023-07-16 12:28:21 +0900

    [Feat] MyResultPage init

---
+++ client/src/components/MyPage/MyResultPage.js
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import {useParams} from 'react-router-dom';
+import {Button, Form} from 'react-bootstrap';
+axios.default.withCredentials = true;
+
+function MyResultPage(props) {
+  const {postId} = useParams();
+  const [post, setPost] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+   useEffect(() => {
+                   localStorage.setItem("cast",1);
+                   localStorage.setItem("rank",-1);
+                   localStorage.setItem("vest",1);
+                   document.cookie = 'cookieName=JSESSIONID; expires=THU, 01 Jan 1970 00:00:00 UTC; path=/;'
+              },[]);
+
+  useEffect(() => {
+    const fetchPost = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:8080/api/members/trip/result?id=${postId}`,
+          {
+            withCredentials: true,
+          }
+        );
+        const postData = response.data;
+        setPost(postData);
+        setLoading(false);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchPost();
+  }, [postId]);
+
+    //댓글 평점
+    const [Review, setReview] = useState("")
+    const [Grade, setGrade] = useState([false,false,false,false,false])
+
+    const reviewChangeHandler = (event) => {
+        setReview(event.currentTarget.value)
+    }
+
+    const gradeChangeHandler = (event) => {
+        let gradeStates = [...Grade];
+
+        for(let i =0; i < 5; i++)
+        {
+            //gradeStates[i] = i <= index ? true : false;
+        }
+
+        setGrade(gradeStates);
+    }
+
+    const setStar = () => {
+        let star = Grade.filter(Boolean).length;
+    }
+
+  return (
+  <div>
+    <div>
+      <h5>시작날짜: {post.startingDate}</h5>
+      <h5>기간: {post.period}</h5>
+      <h5>내용: {post.content}</h5>
+    </div>
+    <div>
+      <h5>댓글</h5>
+        <input type="text" onChange = {reviewChangeHandler} />
+    </div>
+   </div>
+  );
+}
+
+export default MyResultPage;
