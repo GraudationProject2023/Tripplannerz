c6d06dd8c7edc9e95d961f3582fa07aee916d311
YongHyunHong <108213846+YongHyunHong@users.noreply.github.com>
2023-08-15 14:22:07 +0900

    [fix] 어노테이션 추가

--- src/main/java/GraduationProject/TripPlannerZ/service/NotificationService.java
+++ src/main/java/GraduationProject/TripPlannerZ/service/NotificationService.java
@@ -1,7 +1,9 @@
 package GraduationProject.TripPlannerZ.service;
 
+import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+@Service
 public class NotificationService {
     public SseEmitter subscribe(Long id, String lastEventId) {
 
