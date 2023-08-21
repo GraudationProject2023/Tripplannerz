4831ebeac24ccf3ad7bbde5ea82a30a76b2f9c72
Lee-Dongwook <dlehddnr0713@gmail.com>
2023-08-21 18:12:10 +0900

    [feat] Recoil-persist

--- client/src/util/recoilState.js
+++ client/src/util/recoilState.js
@@ -1,11 +1,18 @@
 import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
 
 export const notificationsCountState = atom({
   key: "notificationsCountState",
   default: 0,
 });
 
+const { persistAtom } = recoilPersist({
+  key: "eventSourceLocal",
+  storage: localStorage,
+});
+
 export const eventSource = atom({
   key: "eventSource",
-  default: new EventSource(`http://localhost:8080/api/sub`),
+  default: undefined,
+  effects_UNSTABLE: [persistAtom],
 });
