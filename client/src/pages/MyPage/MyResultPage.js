4f26d2345c8e693a7d2cdff9d8980b3bcee5bb4b
Lee-Dongwook <dlehddnr0713@gmail.com>
2023-10-21 17:42:40 +0900

    ref(StarRating) 파일 구조 변경

--- client/src/pages/MyPage/MyResultPage.js
+++ client/src/pages/MyPage/MyResultPage.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import { Button, Form, Card, Container, Row, Col } from "react-bootstrap";
-import StarRating from "./utils/StarRating";
+import StarRating from "../../util/StarRating"
 
 axios.defaults.withCredentials = true;
 
