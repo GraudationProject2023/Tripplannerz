25dc9a2d7293310fad0f60a2198e06891ac6f185
Lee-Dongwook <dlehddnr0713@gmail.com>
2023-10-22 15:59:17 +0900

    ref(function) Route.js util init

---
+++ client/src/util/Route.js
@@ -0,0 +1,11 @@
+export const moveToBill = (e) => {
+    window.location.href ="/bill"
+}
+
+export function moveToMy() {
+    window.location.href = "/my"
+}
+
+export function moveToMain() {
+    window.location.href = "/main"
+}
