c2697d252facac26bb8f872bfb22333884993f93
Lee-Dongwook <dlehddnr0713@gmail.com>
2023-10-23 09:48:02 +0900

    feat(Calendar) 여행 경비 계산 페이지 > 캘린더 init

--- client/src/pages/BillingPage/BillingPage.js
+++ client/src/pages/BillingPage/BillingPage.js
@@ -1,39 +1,19 @@
 import React, { useState } from 'react';
 import NavBar from '../../components/Navbar/Navbar';
+import { TripCalendar } from '../../components/Calendar/Calendar';
 import './BillingPage.css';
 
 const ExpenseCalculator = () => {
-  const [totalExpense, setTotalExpense] = useState(0);
-  const [expenseInput, setExpenseInput] = useState('');
-
-  const handleExpenseChange = (e) => {
-    setExpenseInput(e.target.value);
-  };
-
-  const addExpense = () => {
-    if (isNaN(expenseInput)) {
-      alert('숫자를 입력해주세요!');
-      return;
-    }
-
-    const newExpense = parseFloat(expenseInput);
-    setTotalExpense((prevTotal) => prevTotal + newExpense);
-    setExpenseInput('');
-  };
+  const [currentDate, setCurrentDate] = useState(new Date())
 
   return (
     <div>
       <NavBar />
-      <div className='Bill'>
-      <h1>여행 경비 계산기</h1>
-      <p>총 경비: {totalExpense}원</p>
-      <input
-        type="text"
-        value={expenseInput}
-        onChange={handleExpenseChange}
-        placeholder="경비를 입력하세요"
+      <div className='Bill'> 
+      <TripCalendar 
+        onChange={setCurrentDate} 
+        value={currentDate} 
       />
-      <button onClick={addExpense}>추가</button>
       </div>
     </div>
   );
