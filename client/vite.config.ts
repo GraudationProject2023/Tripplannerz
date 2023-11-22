2a02ce77c07fc1ddbeaeb716b9085573f599f167
Lee-Dongwook <dlehddnr0713@gmail.com>
2023-11-22 11:11:14 +0900

    feat(config) TailwindCss , postcss 설정

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import WindiCSS from 'vite-plugin-windicss';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default ({mode}) => {
+
+  const env = loadEnv(mode, process.cwd(), "");
+  
+  return defineConfig({
+    define: {
+      "process.env": env,
+
+    },
+    plugins: [react(), WindiCSS()],
+    server: {
+      host: true,
+      port: 3000,
+    },
+    resolve: {
+      alias: [
+        {find: "@", replacement: "/src"}
+      ]
+    }
+  })
+}
