988a3be24c6befe88204ac9876c0e93e122c76c9
Lee-Dongwook <dlehddnr0713@gmail.com>
2023-11-22 11:15:07 +0900

    feat(vite-env.d.ts)

--- client/src/vite-env.d.ts
+++ client/src/vite-env.d.ts
@@ -1,1 +1,9 @@
 /// <reference types="vite/client" />
+
+interface ImportMetaEnv {
+    readonly VITE_CUSTOM_ENV_VARIABLE: string;
+}
+  
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
