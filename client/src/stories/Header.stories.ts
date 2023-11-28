898b9398718ed1a20bb871cb8022003c5a5b6d3f
Lee-Dongwook <dlehddnr0713@gmail.com>
2023-11-28 23:42:15 +0900

    test(hotfix)

--- client/src/stories/Header.stories.ts
+++ client/src/stories/Header.stories.ts
@@ -5,7 +5,6 @@ import { Header } from './Header';
 const meta = {
   title: 'Example/Header',
   component: Header,
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   tags: ['autodocs'],
   parameters: {
     // More on how to position stories at: https://storybook.js.org/docs/react/configure/story-layout
